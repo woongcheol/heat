84530ca1c7c64849c1508772bc6b1c95a5247653
woongcheol <1994_100@naver.com>
2022-01-09 23:12:08 +0900

    add photo tag

--- heat/instagram/admin.py
+++ heat/instagram/admin.py
@@ -1,15 +1,24 @@
 from django.contrib import admin
 from .models import Post
+from django.utils.safestring import mark_safe
+
 # Register your models here.
 
 # admin.site.register(Post)
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['id', 'message', 'message_length', 'is_public', 'created_at', 'updated_at']
+    list_display = ['id', 'photo_tag', 'message', 'message_length', 'is_public', 'created_at', 'updated_at']
     list_display_links = ['message']
     list_filter = ['created_at', 'is_public']
     search_fields = ['message']
+
+    def photo_tag(self, post):
+        if post.photo:
+            return mark_safe(f'<img src="{post.photo.url}" style="width: 72px;" />')
+        return None
+
+        return len(post.message)
 
     def message_length(self, post):
         # return len(post.message)
