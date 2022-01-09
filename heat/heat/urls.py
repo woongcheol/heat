b94fb6a691f6fa80479750cbb03252c0edf712dc
woongcheol <1994_100@naver.com>
2022-01-09 23:07:40 +0900

    update photo url

--- heat/heat/urls.py
+++ heat/heat/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 # from django.conf import global_settings
@@ -24,5 +25,5 @@ urlpatterns = [
     path('instagram/', include('instagram.urls')),
 ]
 
-settings.MEDIA_URL
-settings.MEDIA_ROOT
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
